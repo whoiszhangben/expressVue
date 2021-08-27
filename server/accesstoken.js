21ef0194650a1f371e0de01d7dca058d97748000
caiozhang <caiozhang@tencent.com>
2021-08-27 11:51:43 +0800

    feat: 缓存token

--- server/accesstoken.js
+++ server/accesstoken.js
@@ -1,30 +1,45 @@
 const axios = require('axios');
 const Config = require('../main.config');
+const fs = require('fs');
+const path = require('path');
 
-let AccessToken = function(secret) {
-    const tempSecret = secret;
+let AccessToken = function(secretType) {
+    const temp = secretType || 'common';
     return {
-        getToken : async () => {
+        _isExpire(create_time, expire_time) {
+            const current = Math.floor(Date.now() / 1000);
+            return create_time + expire_time < current;
+        },
+        async getToken () {
+            const token = JSON.parse(fs.readFileSync(path.join(__dirname, '../cache/token.json'), 'utf8'));
+            // return 'lXRg_-GSu_twnNQ78ipfy9M9FbrGbNCTvG83XGnfCu-OAb8vkv59ICud4VDcP8xZoz50yV71jU3_UsJ-peLju1RQjDidPeHalLYt1KwQiNfEy0nD22w_hFYMrsanXQKo162pvQ3aN6deZPAtNppb1-0yI_DM088udQq7wmyreEdnWPCQegAsCkzC7N5QJl-LapZv2VM5DbStQ6CzHj9cQw';
+            if(this._isExpire(token[temp].create_time, token[temp].expire_time)) {
+                let corpid = Config.corpid;
+                let corpsecret = Config.secret[temp] || Config.secret.common;
+            
+                const {data: {
+                    access_token, expires_in
+                }} = await axios.get('https://qyapi.weixin.qq.com/cgi-bin/gettoken', {
+                    params: {
+                        corpid,
+                        corpsecret
+                    }
+                });
+                token[temp].create_time = Math.floor(Date.now() / 1000);
+                token[temp].expire_time = expires_in;
+                token[temp].token = access_token;
+                fs.writeFileSync(path.join(__dirname, '../cache/token.json'), JSON.stringify(token), {
+                    encoding: 'utf-8'
+                });
+                console.log('-----------------------');
+                console.log('Access Token OK!');
+                console.log(access_token);
+                console.log('-----------------------');
+                return access_token;
+            } else {
+                return token[temp].token;
+            }
 
-            return 'lXRg_-GSu_twnNQ78ipfy9M9FbrGbNCTvG83XGnfCu-OAb8vkv59ICud4VDcP8xZoz50yV71jU3_UsJ-peLju1RQjDidPeHalLYt1KwQiNfEy0nD22w_hFYMrsanXQKo162pvQ3aN6deZPAtNppb1-0yI_DM088udQq7wmyreEdnWPCQegAsCkzC7N5QJl-LapZv2VM5DbStQ6CzHj9cQw';
-        
-            let corpid = Config.corpid;
-            let corpsecret = tempSecret || Config.secret;
-        
-            const {data: {
-                access_token
-            }} = await axios.get('https://qyapi.weixin.qq.com/cgi-bin/gettoken', {
-                params: {
-                    corpid,
-                    corpsecret
-                }
-            });
-    
-            console.log('-----------------------');
-            console.log('Access Token OK!');
-            console.log(access_token);
-            console.log('-----------------------');
-            return access_token;
         }
     }
 };
