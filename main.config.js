f3213021ff5835019521958e2e4ec907d4562b88
meterscao <meterscao@tencent.com>
2021-08-27 16:25:50 +0800

    feat: ingnore some file

--- main.config.js
+++ main.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    corpid:'ww827c549fa062654e',
+    corpid:'YOUR_CORP_ID',
     secret:{
-        common: 'usQ8RRMOva7IKPXzxMiwevit-hQ30i3-Eh04XjX5u5E',
-        contact: 'j_C5uQpJbSZ9uqKB6IieIBorQHQovcKUkQJI85ZzvSg'
+        common: 'YOUR_APP_SECRET',
+        contact: 'YOUR_CONTACT_SECRET'
     },
-    robot:'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c3f73dde-bac6-46ad-8047-06dde1c001c8',
+    robot:'YOUR_ROBOT_WEBHOOK_URL',
 }
